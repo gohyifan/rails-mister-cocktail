bd061bd3b80c9284b74e6ea44578ab28377fc22a
Yi Fan Goh <gohyf91@gmail.com>
2019-11-22 16:45:20 +0800

    Updated routes & show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:show, :new, :create]
 
-  # root to: 'cocktails#index'
-  get '/', to: 'cocktails#index'
+  root to: 'cocktails#index'
 
   post 'cocktails/:cocktail_id/doses', to: 'doses#create', as: 'cocktail_doses'
   delete '/doses', to: 'doses#destroy', as: 'delete_doses'
